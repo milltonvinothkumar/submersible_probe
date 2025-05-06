d3c1c9007227fd8c89c3c305493bd878f0ca4bec
vinothm <vinothm@maveric-systems.com>
2025-05-07 00:51:37 +0530

    added pojo for position

---
+++ probedemo/src/main/java/com/example/probedemo/model/Position.java
@@ -0,0 +1,15 @@
+package com.example.probedemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Position {
+
+    private int x;
+    private int y;
+    private Direction direction;
+}
